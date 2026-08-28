6cbfbf90da84c2abec1bacee1a614cea3679f967
Joseph Guenther <joseph@codingcool.ca>
2026-08-28 08:42:55 -0400

    Minor upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://openvb.net',
   integrations: [
     starlight({
       title: 'OpenVB Docs',
